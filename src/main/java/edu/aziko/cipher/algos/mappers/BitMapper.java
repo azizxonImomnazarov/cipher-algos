64d9772e79153729f951d02826ad61f8f07da03f
Azizxon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-01-31 00:19:44 +0500

    delete BitMapper class

--- src/main/java/edu/aziko/cipher/algos/mappers/BitMapper.java
+++
@@ -1,9 +0,0 @@
-package edu.aziko.cipher.algos.mappers;
-
-public class BitMapper {
-
-    public long stringToBit8(String text) {
-        long textInBit8 = 0L;
-        return textInBit8;
-    }
-}
