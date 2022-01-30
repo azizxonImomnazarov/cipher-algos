235ecd081f07b29964d6375c6012983ae0407ce1
Azizxon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-01-31 00:25:50 +0500

    added KEY field

--- src/main/java/edu/aziko/cipher/algos/vernam/VernamCipher.java
+++ src/main/java/edu/aziko/cipher/algos/vernam/VernamCipher.java
@@ -2,6 +2,12 @@ package edu.aziko.cipher.algos.vernam;
 
 public class VernamCipher {
 
+    private final String KEY;
+
+    public VernamCipher(String key) {
+        this.KEY = key;
+    }
+
     public String crypt(String plainText) {
         String cipherText = "";
         return cipherText;
