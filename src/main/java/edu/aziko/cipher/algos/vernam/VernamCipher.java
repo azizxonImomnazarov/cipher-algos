c590c0067f89be277a9dae51b4315c6f9cfd38ea
Azizxon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-01-31 00:00:24 +0500

    added to VernamCipher class method crypt

--- src/main/java/edu/aziko/cipher/algos/vernam/VernamCipher.java
+++ src/main/java/edu/aziko/cipher/algos/vernam/VernamCipher.java
@@ -2,4 +2,8 @@ package edu.aziko.cipher.algos.vernam;
 
 public class VernamCipher {
 
+    public String crypt(String plainText) {
+        String cipherText = "";
+        return cipherText;
+    }
 }
