46a6c1c4d23de00b66291dfd60eb5a19c2509110
Azizxon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-02-01 01:07:57 +0500

    added decodeFromStringToIntStream method

--- src/main/java/edu/aziko/cipher/algos/mapper/StreamDecoder.java
+++ src/main/java/edu/aziko/cipher/algos/mapper/StreamDecoder.java
@@ -3,10 +3,14 @@ package edu.aziko.cipher.algos.mapper;
 import java.util.stream.IntStream;
 
 public class StreamDecoder {
-    public static String decodeFromIntStreamToString(int[] decryptedTextStream) {
-        return IntStream.of(decryptedTextStream)
+    public static String decodeFromIntStreamToString(int[] intStream) {
+        return IntStream.of(intStream)
                 .collect(StringBuilder::new,
                         StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
+    }
+
+    public static int[] decodeFromStringToIntStream(String text) {
+        return text.chars().toArray();
     }
 }
