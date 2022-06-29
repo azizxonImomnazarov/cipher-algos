06b896686484f15db2f7823ee29b9fef19c18ad0
Azizkhon Imomnazarov <azizxonmamurovich202@gmail.com>
2022-06-29 03:50:43 -0700

    minor changes

---
+++ src/test/java/algos/Test1.java
@@ -0,0 +1,13 @@
+package algos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test1 {
+
+    @Test
+    public void test() {
+        assertTrue(false);
+    }
+}
